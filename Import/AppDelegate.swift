1e25945a43ee1a6c221660e9ba7cd2984939034d
Marko Hlebar <marko.hlebar@gmail.com>
2016-10-04 21:38:05 +0200

    Initial Commit

---
+++ Import/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Import
+//
+//  Created by Marko Hlebar on 04/10/2016.
+//  Copyright © 2016 Marko Hlebar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
