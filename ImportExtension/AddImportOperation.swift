574e127f997fb712285a4cd7dccb518496545375
lixy <ky@keepyounger.com>
2016-10-24 11:32:17 +0800

    Bug fix for #7

--- ImportExtension/AddImportOperation.swift
+++ ImportExtension/AddImportOperation.swift
@@ -42,6 +42,11 @@ class AddImportOperation {
         
         self.buffer.lines.removeObject(at: selectionLine)
         self.buffer.lines.insert(importString, at: line)
+        
+        //add a new selection. Bug fix for #7
+        let selectionPosition = XCSourceTextRange.init(start: XCSourceTextPosition.init(line: 0, column: 0), end: XCSourceTextPosition.init(line: 0, column: 0))
+        self.buffer.selections.removeAllObjects()
+        self.buffer.selections.insert(selectionPosition, at: 0)
     }
     
     func isValid(importString: String) -> Bool {
