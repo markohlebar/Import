db45b41436939c09e21a26c1da35f1d7c13537c5
Marko Hlebar <marko.hlebar@gmail.com>
2018-09-30 21:17:49 +0200

    build fixed for app store settings.

--- Import/Windows/RootWindowController.swift
+++ Import/Windows/RootWindowController.swift
@@ -13,7 +13,8 @@ import AVKit
 fileprivate enum Resources {
     static let keyBindingsPath = "/Library/Developer/Xcode/UserData/KeyBindings/Default.idekeybindings"
     static let systemPrefsScript = (name: "open_system_preferences", extension:"scpt")
-    static let usageMovie = (name: "usage", extension: "mov")
+    static let usageMovie = (name: "usage", extension: "mp4")
+    static let installationMovie = (name: "installation", extension: "mp4")
 }
 
 class RootWindowController: NSWindowController {
@@ -29,6 +30,15 @@ class RootWindowController: NSWindowController {
         inserter.insertBindings()
     }
     
+    @IBAction func switchInstructions(_ sender: NSSegmentedControl) {
+        playItem(atUrl: movieUrl(for: sender.selectedSegment))
+    }
+
+    func movieUrl(for segment: Int) -> URL {
+        let file = segment == 0 ? Resources.installationMovie : Resources.usageMovie
+        return Bundle.main.url(forResource: file.name, withExtension: file.extension)!
+    }
+
     @IBAction func openSystemPreferences(_ sender: AnyObject) {
         let file = Resources.systemPrefsScript
         let url = Bundle.main.url(forResource: file.name, withExtension: file.extension)!
@@ -39,14 +49,17 @@ class RootWindowController: NSWindowController {
     override func windowDidLoad() {
         super.windowDidLoad()
 
-        let file = Resources.usageMovie
-        let url = Bundle.main.url(forResource: file.name, withExtension: file.extension)!
+        let url = movieUrl(for: 0)
+        playItem(atUrl: url);
+    }
+
+    func playItem(atUrl url: URL) {
         let player = AVPlayer(url: url)
         player.actionAtItemEnd = .none
         player.play()
-        
+
         playerView.player = player
-        
+
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(loop(with:)),
                                                name: Notification.Name.AVPlayerItemDidPlayToEndTime,
